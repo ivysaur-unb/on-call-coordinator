c9f9e40c8c2042685c9b62115e2cd8c5507c124e
Colby <cfoster3@unb.ca>
2022-10-08 15:36:46 -0300

    More UI changes

--- client/src/TeacherProfile.js
+++ client/src/TeacherProfile.js
@@ -1,36 +1,71 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import {Checkbox, FormControlLabel, FormGroup, TextField, FormLabel, Button} from '@mui/material';
+import {Checkbox, FormControlLabel, FormGroup, TextField, FormLabel, Button, Autocomplete} from '@mui/material';
 import './TeacherProfile.css';
 
+
+
 export class TeacherProfile extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            courses: ['TESTC'],
+            schedule: [[], [], [], [], []]
+        }
+        this.handleSubmission = this.handleSubmission.bind(this);
+    }
+
+    onTextChange = event => {
+        this.setState({ name: event.target.value });
+    }
+
+    handleSubmission = event => {
+        //alert(this.state.name);
+        //this.state({name: event.target.value});
+        this.setState({courses: event.target.value})
+        alert(event.target.value)
+    }
+
+    handleAddCourses = event => {
+        //alert(this.state.courses)
+    }
+
     render(){
         return (
             <div className='root'>
                 
-                <form className='form'>
+                <form className='form' onSubmit={this.handleSubmission}>
                     <label>Teacher Information Form</label>
                     <div >
                         <TextField 
                             id="outlined-basic"
                             label="Name" 
                             variant="outlined"
-                            type="text" 
+                            type="text"
+                            onChange={this.onTextChange}
                         />
                     </div>    
                     <div>
-                        <FormLabel component="legend">Skills</FormLabel>
-                        <FormGroup>
-                            <FormControlLabel control={<Checkbox/>} label='Math'/>
-                            <FormControlLabel control={<Checkbox/>} label='English'/>
-                            <FormControlLabel control={<Checkbox/>} label='Physed'/>
-                            <FormControlLabel control={<Checkbox/>} label='History'/>
-                            <FormControlLabel control={<Checkbox/>} label='Science'/>
-                        </FormGroup>
+                        <Autocomplete
+                            multiple
+                            id="tags-outlined"
+                            className='auto'
+                            size='small'
+                            options={courses}
+                            getOptionLabel={(option) => option.label}
+                            filterSelectedOptions
+                            renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Courses"
+                                placeholder="Select Courses"
+                            />
+                            )}
+                        />
                     </div>
-                    <div>
-                        
-                    </div>
+                    
                     <div>
                         <Button type='submit'>Submit</Button>
                     </div>
@@ -39,3 +74,7 @@ export class TeacherProfile extends React.Component{
         );
     }
 }
+const courses = [
+    {label: 'Math'},
+    {label: 'English'},
+];
