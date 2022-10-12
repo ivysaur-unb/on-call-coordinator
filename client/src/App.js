a79205f61c3ad377d4e5b5c72eff61b5ebf5d2e3
Conner-H <80410507+Conner-H@users.noreply.github.com>
2022-10-12 19:13:49 -0300

    made a few style changes, working on getting a proper title.

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,16 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 function App() {
 
   return (
+    
    <div>
+    <head> <title> On Call Coordinator </title> </head>
+      <center>On Call Coordinator Landing Page</center>
       <Router>
         <Routes>
 
           <Route path='/' element={
             <ul>
-              <li><a href='/teacherAbsences'>Teacher Absences</a></li>
+              <li><button href='/teacherAbsences'>Teacher Absences</button></li>
             </ul>
           }/>
           <Route path='/teacherAbsences' element={<TeacherAbsences/>}/>
