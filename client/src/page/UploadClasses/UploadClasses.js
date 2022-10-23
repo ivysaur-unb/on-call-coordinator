36014bd3f37723ceb13ce117c3d3682848cf5410
Grace Kiely <gkkiely11@gmail.com>
2022-10-23 14:24:15 -0300

    working so far

--- client/src/page/UploadClasses/UploadClasses.js
+++ client/src/page/UploadClasses/UploadClasses.js
@@ -4,9 +4,11 @@ import readXlsxFile from "read-excel-file";
 export default function UploadClasses() {
     const handleupload = (e) => {
         console.log(e.target.files[0]);
+        //console.log(e.target.files[0][1]);
         readXlsxFile(e.target.files[0]).then((rows) => {
-            console.log(rows);
+            console.log(rows[0][1]);
         });
+        //console.log(e.target.files[1][2]);
     };
 
     return (
---
+++ server/prisma/migrations/20221023161500_adding_absence/migration.sql
@@ -0,0 +1,93 @@
+/*
+  Warnings:
+
+  - Added the required column `schoolId` to the `Teacher` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX `Teacher_initials_key` ON `Teacher`;
+
+-- AlterTable
+ALTER TABLE `Teacher` ADD COLUMN `schoolId` INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Teachable` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `School` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `numberOfStudents` INTEGER NOT NULL,
+    `specialityPrograms` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Schedule` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `teacherId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Schedule_teacherId_key`(`teacherId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ScheduledClass` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `scheduleId` INTEGER NOT NULL,
+    `period` INTEGER NOT NULL,
+    `classId` INTEGER NULL,
+    `specialCode` ENUM('MONITORING', 'RESOURCE_ROOM', 'LIBRARY', 'CHAPLAINCY', 'GUIDANCE', 'STUDENT_SUCCESS', 'NOT_AVAILABLE') NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Class` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `courseCode` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `grade` ENUM('NINE', 'TEN', 'ELEVEN', 'TWELVE', 'SECONDARY', 'LEVEL_1', 'LEVEL_2', 'LEVEL_3', 'LEVEL_4') NOT NULL,
+    `pathway` ENUM('COLLEGE_PREP', 'OPEN', 'WORK_PREP', 'ACADEMIC', 'APPLIED') NOT NULL,
+    `teachableId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Class_courseCode_key`(`courseCode`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_TeachableToTeacher` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_TeachableToTeacher_AB_unique`(`A`, `B`),
+    INDEX `_TeachableToTeacher_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Teacher` ADD CONSTRAINT `Teacher_schoolId_fkey` FOREIGN KEY (`schoolId`) REFERENCES `School`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Schedule` ADD CONSTRAINT `Schedule_teacherId_fkey` FOREIGN KEY (`teacherId`) REFERENCES `Teacher`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ScheduledClass` ADD CONSTRAINT `ScheduledClass_scheduleId_fkey` FOREIGN KEY (`scheduleId`) REFERENCES `Schedule`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ScheduledClass` ADD CONSTRAINT `ScheduledClass_classId_fkey` FOREIGN KEY (`classId`) REFERENCES `Class`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Class` ADD CONSTRAINT `Class_teachableId_fkey` FOREIGN KEY (`teachableId`) REFERENCES `Teachable`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_TeachableToTeacher` ADD CONSTRAINT `_TeachableToTeacher_A_fkey` FOREIGN KEY (`A`) REFERENCES `Teachable`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_TeachableToTeacher` ADD CONSTRAINT `_TeachableToTeacher_B_fkey` FOREIGN KEY (`B`) REFERENCES `Teacher`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
