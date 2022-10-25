a6c7338e5e3d6ac34f35482eb1d1e10643832a11
Grace Kiely <gkkiely11@gmail.com>
2022-10-25 15:49:58 -0300

    deleted unneeded

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/courses', function(req, res, next) {
-  res.render('courses', { title: 'Express' });
-});
-
 module.exports = router;
