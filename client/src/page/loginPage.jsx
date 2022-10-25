77c205e9d2a75d78e9b27e42a190bdbcaebbc333
ugyen <uzangmo@Unb.ca>
2022-10-25 18:01:37 -0300

    stores token in session storage

--- client/src/page/loginPage.jsx
+++ client/src/page/loginPage.jsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import  '../page/loginPage.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "../page/loginPage.css";
+import { login } from "../backend-requests/login";
+
 function Login() {
+  function testFunction() {
+    const email = document.querySelector("#email-input-field");
+    const passw = document.querySelector("#filled-password-input");
+    if (email == null || passw == null) {
+      return;
+    }
+    login(email.value, passw.value).then((response) => {
+      response.json().then((response) => {
+        sessionStorage.setItem("token", response.token);
+      });
+    });
+  }
+  return (
+    <form className="login">
+      <div className="page-header">Login</div>
+      <TextField
+        id="email-input-field"
+        label="Email"
+        autoComplete="current-password"
+      />
+      <TextField
+        id="filled-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+      />
 
-
-    return (
-
-    <div className='login'>
-    
-    <div className='page-header'>
-        Login
-    </div>
-    <TextField
-          id="email-input-field"
-          label="Email"
-          autoComplete="current-password"
-          
-    />
-    <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          
-    />
-
-    <Button variant="contained">Submit</Button>
-
-
-    </div>
-    )
-
-
+      <Button variant="contained" onClick={testFunction}>
+        Submit
+      </Button>
+    </form>
+  );
 }
 
 export default Login;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
 const { PrismaClient } = require('@prisma/client')
+
+dotenv.config();
 
 const prisma = new PrismaClient()
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
   const allMyUsers = await prisma.user.findMany()
-  console.log(allMyUsers);
   res.send(allMyUsers)
 });
+
+async function tokenify (user) {
+
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  const token = jwt.sign(user, jwtSecretKey, {expiresIn: '100s'});
+  return token;
+
+}
+
+async function getUserByEmail (email, password) {
+
+ let result = await prisma.user.findFirst({
+    where:{
+      email:email,
+      password:password
+    }
+
+  })
+
+  return result;
+}
+ 
+
+router.post('/login', async function (req, res, next) {
+
+  const user = await getUserByEmail(req.body.email, req.body.password);
+  const token =await tokenify(user);
+
+  res.send({token:token});
+
+})
 
 module.exports = router;
