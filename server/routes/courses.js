d98dd50bc99ee744a3f865a4da6e90caddf55d50
Grace Kiely <gkkiely11@gmail.com>
2022-10-26 19:23:32 -0300

    properly importing createTeachables

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-import { createTeachables } from '../helpers/createTeachables';
+const { createTeachables } = require('../helpers/createTeachables');
 
 const { PrismaClient } = require('@prisma/client');
 
