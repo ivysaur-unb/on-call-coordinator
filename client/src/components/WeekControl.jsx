0a73f36341f9b9275d2b13793ec72756595b298c
Cameron Fiander <cfiande1@unb.ca>
2022-10-25 23:18:12 -0300

    Make component WeekControl

--- client/src/components/WeekControl.jsx
+++ client/src/components/WeekControl.jsx
@@ -1,10 +1,45 @@
 import React, { useState, useEffect } from 'react';
-class WeekControl extends React.Component {
+import { getWeekStart } from '../Helper/Date';
+import {IconButton} from '@mui/material';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import './weekControl.css';
 
-    //Requirements:
-    // Display human-readable date format centered in the component
-    // {Monday {monday Date} - Friday {friday Date}}
-    // Arrows on either side of text increment/decrement week on click
-    // Option to disable going earlier than current week
-    // 'onWeekChange(Date)' function passed into component that can be called to update state of related components
+function WeekControl(props) {
+    const [weekStart, setWeekStart] = useState(getWeekStart())
+
+    useEffect(() => {
+        if(typeof props.onChange === 'function') {
+            props.onChange(weekStart);
+        }
+    }, [props, weekStart])
+
+    function incrementWeekStart(){
+        let nextWeek = new Date(weekStart);
+        nextWeek.setDate(nextWeek.getDate() + 7);
+        setWeekStart(new Date(nextWeek));
+    }
+
+    function decrementWeekStart() {
+        if(weekStart < Date.now()) {
+            return;
+        }
+        let prevWeek = new Date(weekStart);
+        prevWeek.setDate(prevWeek.getDate() - 7);
+        setWeekStart(prevWeek);
+    }
+
+
+    return (
+    <div className='weekControl'>  
+    <IconButton onClick={decrementWeekStart} > <ArrowCircleLeftIcon /> </IconButton>
+        <p>
+            {weekStart.toDateString()} - {new Date(new Date(weekStart).setDate(weekStart.getDate() + 4)).toDateString()}
+        </p>
+    <IconButton onClick={incrementWeekStart}> <ArrowCircleRightIcon /> </IconButton>
+    </div>
+    )
+
 }
+
+export default WeekControl;
