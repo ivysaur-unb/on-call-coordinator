575f57b28511c8f8e6dd86a3b27eaba72394f575
ugyen <uzangmo@Unb.ca>
2022-11-08 22:43:42 -0400

    did more stuffs for getAvailability. 75% done i think

--- server/persist/availabilityTest.test.js
+++ server/persist/availabilityTest.test.js
@@ -3,7 +3,7 @@ const getAvailability = require('../routes/getAvailability');
 
 test ('get all teachers', async function () {
 
-    const teachers = await getAvailability.getFreePeriods();
+    const teachers = await getAvailability.getAvailablePeriods();
     console.log(teachers);
 }
 
--- server/routes/getAvailability.js
+++ server/routes/getAvailability.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-
+//gets all the teachers from the Teacher table
 async function getAllTeachers() {
 
     const allTeachers = await prisma.Teacher.findMany();
@@ -9,11 +9,25 @@ async function getAllTeachers() {
 
 }
 
+//gets all the schedule from Schedule table
+async function getAllSchedule(){
+    const allSchedule = await prisma.Schedule.findMany();
+    return allSchedule;
+}
+
+//gets all the schedule from the ScheduledClass table
 async function getAllScheduledClass() {
     const allSchedule = await prisma.ScheduledClass.findMany();
     return allSchedule;
 }
 
+//gets all the absences from the absence table
+async function getAbsences() {
+    const absences = await prisma.Absence.findMany();
+    return absences;
+}
+
+//returns single schedule id with all the periods associated with it
 async function getScheduleIdAndPeriods () {
 
     const schedule = await getAllScheduledClass();
@@ -34,6 +48,7 @@ async function getScheduleIdAndPeriods () {
     return scheduleIdAndPeriods;
 }
 
+//helper method to return only the free periods 
 async function editPeriods (arr){
 
     const periods = [1,2,3,4];
@@ -49,14 +64,13 @@ async function editPeriods (arr){
     return freePeriods;
 }
 
-
+//returns all the scheduleid and free periods for each teacher
 async function getFreePeriods () {
 
     const schedule = await getScheduleIdAndPeriods();
 
     for (let x in schedule){
 
-       // console.log(x);
         const freePeriod = await editPeriods(schedule[x].periods);
         schedule[x].periods = freePeriod;
 
@@ -66,8 +80,56 @@ async function getFreePeriods () {
 
 }
 
+async function getFreePeriodsWithTeacherId() {
+
+    const schedule = await getAllSchedule();
+    const freePeriods = await getFreePeriods();
+
+    for(let x in freePeriods) {
+
+        let teacherId = schedule.find(t => t.id === freePeriods[x].scheduleId);
+        freePeriods[x].teacherId = teacherId.teacherId;
+    }
+
+    return freePeriods;
+}
+
+function removeAbsences(periodToBeRemoved) {
+
+    return this != periodToBeRemoved;
+}
+//incorporate the data from absence table to edit free periods for each teacher
+async function getAvailablePeriods() {
+
+    const absences = await getAbsences();
+    const freePeriods = await getFreePeriodsWithTeacherId();
+    
+    for (let i = 0; i < absences.length; i++) {
+
+        const temp = absences[i];
+        let temp2;
+
+        for (x in freePeriods) {
+            if (freePeriods[x].teacherId === temp.teacherId) {
+                temp2 = freePeriods[x];
+
+                if (temp2.periods.indexOf(temp.period) != -1) {
+                    temp2.periods = temp2.periods.filter(removeAbsences, temp.period);
+                }
+            }
+        }    
+    }
+
+    return freePeriods;
+}
+
 
 module.exports.getAllTeachers = getAllTeachers;
 module.exports.getAllScheduledClass = getAllScheduledClass;
 module.exports.getScheduleIdAndPeriods = getScheduleIdAndPeriods;
 module.exports.getFreePeriods = getFreePeriods;
+module.exports.getAbsences = getAbsences;
+module.exports.getAllSchedule = getAllSchedule;
+module.exports.getFreePeriodsWithTeacherId = getFreePeriodsWithTeacherId;
+module.exports.getAvailablePeriods = getAvailablePeriods;
+module.exports.removeAbsences = removeAbsences;
