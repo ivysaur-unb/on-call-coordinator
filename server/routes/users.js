59446e9c26387912c49814f8ea313eca0ecfa6a8
MitchellCWM <104988392+MitchellCWM@users.noreply.github.com>
2022-11-10 11:32:19 -0400

    neatened users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,48 +10,48 @@ dotenv.config();
 const prisma = new PrismaClient()
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   const allMyUsers = await prisma.user.findMany()
   res.send(allMyUsers)
 });
 
-async function tokenify (user) {
+async function tokenify(user) {
 
-  
+
   let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  const token = jwt.sign(user, jwtSecretKey, {expiresIn: '100s'});
+  const token = jwt.sign(user, jwtSecretKey, { expiresIn: '100s' });
   return token;
 
 }
 
-async function getUserByEmail (email, password) {
+async function getUserByEmail(email, password) {
 
- let result = await prisma.user.findFirst({
-    where:{
-      email:email,
-      password:password
+  let result = await prisma.user.findFirst({
+    where: {
+      email: email,
+      password: password
     }
 
   })
 
   return result;
 }
- 
+
 
 router.post('/login', async function (req, res, next) {
 
   const user = await getUserByEmail(req.body.email, req.body.password);
-  if (user==null) {
+  if (user == null) {
 
     res.status(401);
     res.send("User does not exist!");
-    
+
   }
 
   else {
-  const token =await tokenify(user);
+    const token = await tokenify(user);
 
-  res.send({token:token});
+    res.send({ token: token });
   }
 })
 
