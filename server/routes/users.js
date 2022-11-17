ca72f451ce2ad8982dc1f13ed03a7bd8a01be6f4
Cameron Fiander <cfiande1@unb.ca>
2022-11-17 10:41:23 -0400

    IVYSAUR-109 Handle invalid login args

--- server/routes/users.js
+++ server/routes/users.js
@@ -35,7 +35,11 @@ async function getUserByEmail(email, password) {
 
 
 router.post('/login', async function (req, res, next) {
-
+  if(!req.body.email || !req.body.password) {
+    res.status(401);
+    res.send("Invalid arguments");
+    return;
+  }
   const user = await getUserByEmail(req.body.email, req.body.password);
   if (user == null) {
 
