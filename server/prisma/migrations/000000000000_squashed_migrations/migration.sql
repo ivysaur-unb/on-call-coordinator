a6dffcb3ff305b26aa3acdadaaaca7e0a44141be
camfiander <73791562+camfiander@users.noreply.github.com>
2022-11-17 02:32:22 -0400

    Fix prisma plz (#24)
    * Fix runtime issues
    
    * Squash prisma migrations
    
    * fix migration

--- server/prisma/migrations/000000000000_squashed_migrations/migration.sql
+++ server/prisma/migrations/000000000000_squashed_migrations/migration.sql
@@ -7,7 +7,6 @@ CREATE TABLE `User` (
     `role` ENUM('USER', 'TEACHER', 'SUPPLY', 'ADMIN') NOT NULL DEFAULT 'USER',
 
     UNIQUE INDEX `User_email_key`(`email`),
-    UNIQUE INDEX `User_password_key`(`password`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
