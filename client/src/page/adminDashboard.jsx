d4f87e8239bd02191147e31954f28be90cb6080b
MitchellCWM <104988392+MitchellCWM@users.noreply.github.com>
2022-11-22 14:17:21 -0400

    made it possible to style the current tab open, no styling done yet

--- client/src/page/adminDashboard.jsx
+++ client/src/page/adminDashboard.jsx
@@ -1,4 +1,4 @@
-import "./adminDashboard.css"
+import "./dashboard.css"
 import adminImage from "./images/admin.png"
 import { useState, useContext } from "react";
 import AbsenceSchedule from './AbsenceSchedule';
@@ -10,27 +10,36 @@ import { TeacherProfile } from './TeacherProfile/TeacherProfile';
 import UploadClasses from './UploadClasses/UploadClasses';
 import SchoolSchedule from "./schoolSchedule";
 function AdminDashboard({ }) {
+
     const user = useContext(UserContext);
-    const [displayImage2, setImage] = useState(false);
-
     const displayImage = <Box sx={{ textAlign: "center" }}><img src={adminImage} alt="AdminImage" /><div>Welcome, {user.name} </div></Box>
-
-
     const [activePage, setActivePage] = useState(displayImage);
+    
+    const setActive = function(element){
+        return (e)=>{
+            let prev = document.querySelector('.dashboard-sidenav .active-tab');
+            if(prev){
+                prev.className = '';
+            }
+            
+            e.target.className = 'active-tab';
+            setActivePage(element);
+        }
+    }
     return (
         <Stack direction={'row'} sx={{height:'100%'}}>
-            <div class="sidenav2">
-                <h3><a href='/' onClick={() => setActivePage(displayImage)}>HOME</a></h3>
+            <nav className="dashboard-sidenav">
+                <h3><header className='active-tab' onClick={() => setActivePage(displayImage)}>HOME</header></h3>
                 <ul>
-                    <li onClick={() => setActivePage(<AbsenceSchedule />)}>Teacher Absences</li>
-                    <li onClick={() => setActivePage(<CreateSchoolForm />)}>Create School</li>
-                    <li  onClick={() => setActivePage(<TeacherProfile/>)}>Add Teacher</li>
-                    <li onClick={() => setActivePage(<UploadClasses/>)}>Upload Courses</li>
-                    <li  onClick={() => setActivePage(<SchoolSchedule/>)}>School Schedule</li>
+                    <li  onClick={setActive(<AbsenceSchedule/>)}>Teacher Absences</li>
+                    <li onClick={setActive(<CreateSchoolForm />)}>Create School</li>
+                    <li onClick={setActive(<TeacherProfile/>)}>Add Teacher</li>
+                    <li onClick={setActive(<UploadClasses/>)}>Upload Courses</li>
+                    <li onClick={setActive(<SchoolSchedule/>)}>School Schedule</li>
                 </ul>
-            </div>
+            </nav>
 
-            <div class="main2">
+            <div className="dashboard-main">
                 {activePage}
             </div>
         </Stack>
