9dca56930cd1f4f230ddb666d964df97bbf01fc9
Conner-H <80410507+Conner-H@users.noreply.github.com>
2022-11-28 09:17:33 -0400

    Revert "pushing the commit issues here"
    This reverts commit 32ebc77425acbeeac998400e1b1c75f870c22a73.

--- client/src/page/adminDashboard.jsx
+++ client/src/page/adminDashboard.jsx
@@ -1,47 +1,28 @@
 import "./adminDashboard.css"
 import adminImage from "./images/admin.png"
 import { useState } from "react";
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import AddBusinessIcon from '@mui/icons-material/AddBusiness';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import UploadIcon from '@mui/icons-material/Upload';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-function AdminDashboard(){
-const [displayImage, setImage] = useState(false);
-    return (
-    <>
-        <div class="sidenav2">
-        <h3>DASHBOARD</h3>
-        <div class="absences">
-        <CalendarMonthIcon fontSize="large"></CalendarMonthIcon>
-        <a target = "frame" href='/importAbsences' onClick={() => setImage(true)}> Teacher Absences</a>
-        </div>
-        <div class="school">
-        <AddBusinessIcon fontSize="large"></AddBusinessIcon>
-        <a target = "frame" href='/board'  onClick={() => setImage(true)}>Create School</a>
-        </div>
-        <div class = "teacher">
-        <PersonAddIcon fontSize="large"></PersonAddIcon>
-        <a target = "frame" href='/addTeacher'  onClick={() => setImage(true)}>Add Teacher</a>
-        </div>
-        <div class = "class">
-        <UploadIcon fontSize="large"></UploadIcon>
-        <a target = "frame" href='/uploadClasses'  onClick={() => setImage(true)}>Upload Classes</a>
-        </div>
+import AbsenceSchedule from './AbsenceSchedule';
+import { Box } from "@mui/system";
 
-        <div class = "schedule">
-        <AccessTimeIcon fontSize="large"></AccessTimeIcon>
-        <a target = "frame" href='/schoolSchedule'  onClick={() => setImage(true)}>School Schedule</a>
-        </div>
-        </div>
-            
-        <div class="main2">
-            {!displayImage ? (<img src= {adminImage} alt = "AdminImage"/>) : null}
-            <iframe name = "frame" title = "main body"></iframe>
-        </div>
-        <settings>
-        </settings>
-    </>
-  );
+function AdminDashboard({user}) {
+    const [displayImage, setImage] = useState(false);
+    return (
+        <>
+            <div class="sidenav2">
+            <h3><a href='/' onClick={() => setImage(false)}>HOME</a></h3>
+                <a target="frame" href='/importAbsences' onClick={() => setImage(true)}>Teacher Absences</a>
+                <a target="frame" href='/board' onClick={() => setImage(true)}>Create School</a>
+                <a target="frame" href='/addTeacher' onClick={() => setImage(true)}>Add Teacher</a>
+                <a target="frame" href='/uploadClasses' onClick={() => setImage(true)}>Upload Courses</a>
+                <a target="frame" href='/schoolSchedule' onClick={() => setImage(true)}>School Schedule</a>
+            </div>
+
+            <div class="main2">
+            {!displayImage ? (<Box sx={{textAlign: "center"}}><img src={adminImage} alt="AdminImage"/><div>Welcome, {user ? user.name : "ADMIN"}</div></Box>) : null}
+                <iframe name="frame" title="main body">
+                </iframe>
+            </div>
+        </>
+    );
 }
 export default AdminDashboard;
