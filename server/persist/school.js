388e324bed375235f86c92615d2bb4172f9a3587
mmacdo52 <97447686+mmacdo52@users.noreply.github.com>
2022-11-29 10:00:40 -0400

    Creating a school will now inform you of its success or failure (26)
    * created error message and success message
    
    * updated the backend for large number of students
    
    * fixed bug where error message would still be displayed while success notifcation was up
    
    * removed commented out code
    
    * cleaned up the backend
    
    * removed a console.log
    
    * added an error message if you try to create a duplicate school, same name, instead of crashing the backend
    
    * the speciality program is now left aligned instead of center
    
    Co-authored-by: MitchellCWM <104988392+MitchellCWM@users.noreply.github.com>

--- server/persist/school.js
+++ server/persist/school.js
@@ -15,4 +15,59 @@ async function createSchool(school) {
   }
 }
 
+async function getSchools() {
+  const allMySchools = await prisma.school.findMany();
+  return allMySchools
+}
+
+//this is the function used by the website on the create school page
+async function postSchools(body) {
+  let name = body.name;
+  let address = body.address;
+  let numberOfStudents = body.numberOfStudents;
+  let specialityPrograms = body.specialityPrograms;
+
+  if(!specialityPrograms || !name || !address || !numberOfStudents ){
+    return 422;
+  }
+ //make sure you have all the parameters
+  if(numberOfStudents < 0){
+    return 422;
+  }
+
+  if(numberOfStudents > 1000000000){
+    return 507;
+  }
+
+  let alrightCreated = await prisma.school.findFirst({
+    where:{
+      name: body.name
+    }
+  })
+  if(alrightCreated != null){
+    //conflicting data error code
+    return 409;
+  }
+  let test = await prisma.school.create({data:{
+      name: body.name,
+      address:body.address,
+      numberOfStudents: body.numberOfStudents,
+      specialityPrograms: body.specialityPrograms
+  }})
+  return test;
+}
+
+async function deleteSchools(body){
+  let response = await prisma.school.delete({
+    where:{
+      id: body.id
+    }
+  })
+
+  return response
+}
+
 module.exports.createSchool = createSchool;
+module.exports.deleteSchools = deleteSchools;
+module.exports.postSchools = postSchools;
+module.exports.getSchools = getSchools;
