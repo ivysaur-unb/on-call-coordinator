753e8c3c5e9049e62cea96843f2cf5a022953e38
Cameron Fiander <cfiande1@unb.ca>
2022-11-28 23:30:49 -0400

    Fix init failure

--- server/init/initializeDatabase.js
+++ server/init/initializeDatabase.js
@@ -166,7 +166,9 @@ async function clearSchedule(teacher) {
       id: true,
     },
   });
-  // teacherSchedule.id may be null here
+  if(!teacherSchedule) {
+    return;
+  }
   await prisma.scheduledClass.deleteMany({
     where: {
       scheduleId: teacherSchedule.id,
