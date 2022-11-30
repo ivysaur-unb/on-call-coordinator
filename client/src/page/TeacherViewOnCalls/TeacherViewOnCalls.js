e3750a0c017d6c6ecdef5c090e55033c20b9debe
Grace Kiely <gkkiely11@gmail.com>
2022-11-29 22:11:19 -0400

    displays logged in teachers oncalls

--- client/src/page/TeacherViewOnCalls/TeacherViewOnCalls.js
+++ client/src/page/TeacherViewOnCalls/TeacherViewOnCalls.js
@@ -1,40 +1,47 @@
 import "./TeacherViewOnCalls.css";
 import React, { useState, useEffect } from 'react';
-import {Checkbox, FormControlLabel, FormGroup, TextField, Box, FormLabel, Button, Autocomplete, Typography} from '@mui/material';
-import {teachables} from '../../Courses';
-import DefaultProfilePicture from '../../default-profile-picture.jpg';
-//import { createTeachables } from "../../../../server/helpers/createTeachables";
+import {Checkbox, FormControlLabel, FormGroup, TextField, Box, FormLabel, Button, Autocomplete, Typography, List, ListItem, ListItemText} from '@mui/material';
 import { myOnCalls } from "../../backend-requests/viewOnCalls";
+
+import { useContext } from "react";
+import { UserContext } from "../../App";
 
 export default function VPViewOnCalls() {
 
-
-
-
-    const [teacher, setTeacher] = useState()
-    const [onCalls, setOnCalls] = useState([])
+    const user = useContext(UserContext);
+    const [onCalls, setOnCalls] = useState({})
 
     useEffect(() => {
-        myOnCalls(1).then(data => {
+        myOnCalls(user.id).then(data => {
           setOnCalls(data)
-          //console.log(data.initials)
         })
       }, []);
-      //TODO: get for currently logged in teacher
-
-      const OC = myOnCalls(1)[0];
-      console.log(myOnCalls(1)[0]);
-
-
-
-
-
+  
 
 
 
     return (
         <div className='root'>
-            <h2>My On-Calls: {OC}</h2>
+            <h2>My On-Calls: </h2>
+            {onCalls.length > 0 &&
+                    <Box>
+                        <div className='oncalls-in'>
+                            <List>
+                            <ListItem ><ListItemText primary="Class"/><ListItemText primary="               "/><ListItemText primary="Day"/></ListItem>
+                                {onCalls.map((val) => {
+                                    return <ListItem ><ListItemText primary={val.scheduledClassId}/><ListItemText primary="----------------"/><ListItemText primary={val.day}/></ListItem>
+                                })}
+                            </List>
+                        </div>
+                    </Box>
+                }    
+                {onCalls.length === 0 &&
+                    
+                        <Typography variant="h5" component="div">
+                            No On-Calls To Display
+                        </Typography>
+                       
+                }  
         </div>
     );
 
