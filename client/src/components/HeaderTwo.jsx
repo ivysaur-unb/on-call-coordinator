c2b7e957bae87851e973544108b4f97c5cacabaf
Conner-H <80410507+Conner-H@users.noreply.github.com>
2022-11-29 20:03:16 -0400

    set up theme stuff for new header

--- client/src/components/HeaderTwo.jsx
+++ client/src/components/HeaderTwo.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { IconButton } from "@mui/material";
+import { IconButton, ThemeProvider } from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { UserContext } from '../App';
 import { useContext } from "react";
+import { theme } from "../page/theme"
 const HeaderTwo = function ({ }) {
     let user = useContext(UserContext);
 
     return (
+        <ThemeProvider theme={theme}>
         <div className="header-two">
             <div></div>
             <div className="header-name">
@@ -18,6 +20,7 @@ const HeaderTwo = function ({ }) {
                 <IconButton ><LogoutIcon /> </IconButton>
             </a>
         </div>
+        </ThemeProvider>
     )
 }
 
