6a9e8db9cd611268469122ad2250190f419d8d12
Grace Kiely <gkkiely11@gmail.com>
2022-11-29 22:01:38 -0400

    scheduledClassId and day displaying

--- client/src/page/VPViewOnCalls/VPViewOnCalls.js
+++ client/src/page/VPViewOnCalls/VPViewOnCalls.js
@@ -4,24 +4,11 @@ import {Checkbox, FormControlLabel, FormGroup, TextField, Box, FormLabel, Button
 import { getTeachers } from "../../backend-requests/viewOnCalls";
 import { getTeacherOnCalls } from "../../backend-requests/viewOnCalls";
 
-
 export default function VPViewOnCalls() {
-
-
-    //const [itemsAutocomplete, setItemsAutocomplete] = useState([])
-    /*const onCourseChange = event => {
-        this.setState({ teachables: [...this.state.teachables, {name: `${event.target.textContent}`}] });
-    }*/
 
     const [teachers, setTeachers] = useState([])
     const [chosenTeacher, setChosenTeacher] = useState()
     const [onCalls, setOnCalls] = useState({})
-
-    const [schedClassId, setSchedClassId] = useState([])
-    const [date, setDate] = useState([])
-    //const [value, setValue] = useState(teachers[0]);
-
-    
 
     useEffect(() => {
         getTeachers().then(data => {
@@ -29,67 +16,16 @@ export default function VPViewOnCalls() {
         })
       }, []);
 
+    const onTeacherChange = event => {
+        setChosenTeacher(document.getElementById("tags-outlined").value)
+        console.log(chosenTeacher);
+    }
 
-
-  
-      /*const handleSubmission = (data) => {
-          /*setTeachers(data.teachers);
-          if(data.errors.length > 0) {
-            console.log(data.errors);
-          }*/
-        //};
-        /*const handleSubmission = (data) => {
-            getTeacherOnCalls(1).then(data => {
-                //setOnCalls(data)
-                console.log(data[0])
-                
-            })
-            // setOnCalls('heyy')
-            /*setTeachers(data.teachers);
-            if(data.errors.length > 0) {
-              console.log(data.errors);
-            }*/
-          //};
-
-        const onTeacherChange = event => {
-            //setChosenTeacher(event.option.initials)
-            setChosenTeacher(document.getElementById("tags-outlined").value)
-
-            //const selectedTeacher = document.getElementById("tags-outlined").value;
-            //console.log(selectedTeacher);
-            console.log(chosenTeacher);
-        }
-        //TODO: get chosen option from box
-
-        const handleClick = (data) => {
-            /*getTeacherOnCalls(1).then(data => {
-                setOnCalls(data)
-                console.log(data)*/
-                
-            //})
-            getTeacherOnCalls(parseInt(chosenTeacher)).then(data => {
-                //setOnCalls(data.result.onCalls)
-                setOnCalls(data)
-                //for data.length
-                console.log(data[0])
-
-                /*setOnCalls(onCalls => ({
-                    ...onCalls,
-                  }));*/
-                //const result = Object.keys(onCalls).map((key) => onCalls[key]);
-                //console.log(result);
-                //console.log(result[0][0]);
-                //setOnCalls(result);
-
-                //setDate(Object.keys(onCalls[0]).map((day) => onCalls[0][day]);
-                
-                
-            })
-            console.log(onCalls)
-            console.log(onCalls[0].day)
-            //if null write no on calls
-            //setOnCalls("hello")
-        }
+    const handleClick = (data) => {
+        getTeacherOnCalls(parseInt(chosenTeacher)).then(data => {
+            setOnCalls(data)
+        })
+    }
 
     return (
         <div className='root'>
@@ -118,26 +54,32 @@ export default function VPViewOnCalls() {
                 
             
             <div>
-            <Button variant="outlined" component="label" onClick={handleClick} sx={{ color: '#153c7a', backgroundColor: 'whitesmoke', borderColor: '#6183ba'}}>
+            <Button variant="outlined" component="label" onClick={handleClick} sx={{ color: '#153c7a', backgroundColor: 'whitesmoke', borderColor: '#6183ba', marginBottom: '80px'}}>
                     View On-Calls
             </Button>
             </div>
-            <div>
-                    <h3>On calls: </h3>
-            </div>
+           
             {onCalls.length > 0 &&
                     <Box>
                         <Typography variant="h5" component="div">
                             On-Calls
                         </Typography>
-                        <div className='teachable-in'>
+                        <div className='oncalls-in'>
                             <List>
+                            <ListItem ><ListItemText primary="Class"/><ListItemText primary="               "/><ListItemText primary="Day"/></ListItem>
                                 {onCalls.map((val) => {
-                                    return <ListItem disablePadding><ListItemText primary={val.day}/></ListItem>
+                                    return <ListItem ><ListItemText primary={val.scheduledClassId}/><ListItemText primary="----------------"/><ListItemText primary={val.day}/></ListItem>
                                 })}
                             </List>
                         </div>
                     </Box>
+                }    
+                {onCalls.length === 0 &&
+                    
+                        <Typography variant="h5" component="div">
+                            No On-Calls To Display
+                        </Typography>
+                       
                 }    
         </div>
     );
