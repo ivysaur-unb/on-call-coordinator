68536376167ee713a13cd88d434fe590a053beef
Cameron Fiander <cfiande1@unb.ca>
2022-11-29 21:19:51 -0400

    Update prisma migrations

--- server/prisma/migrations/20221116222903_create_teacher_fix/migration.sql
+++ server/prisma/migrations/20221116222903_create_teacher_fix/migration.sql
@@ -4,5 +4,8 @@ ALTER TABLE `Teacher` DROP FOREIGN KEY `Teacher_schoolId_fkey`;
 -- AlterTable
 ALTER TABLE `Teacher` MODIFY `schoolId` INTEGER NULL;
 
+-- AlterTable
+ALTER TABLE `User` MODIFY `password` VARCHAR(191) NULL;
+
 -- AddForeignKey
 ALTER TABLE `Teacher` ADD CONSTRAINT `Teacher_schoolId_fkey` FOREIGN KEY (`schoolId`) REFERENCES `School`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
