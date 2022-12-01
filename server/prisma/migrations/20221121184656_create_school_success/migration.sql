0057658cff01c2fc33acfb8ab3d52150f7660ae4
ugyen <uzangmo@Unb.ca>
2022-12-01 14:11:41 -0400

    migrations deleted

--- server/prisma/migrations/20221121184656_create_school_success/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Teacher` DROP FOREIGN KEY `Teacher_schoolId_fkey`;
-
--- AlterTable
-ALTER TABLE `Teacher` MODIFY `schoolId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `Teacher` ADD CONSTRAINT `Teacher_schoolId_fkey` FOREIGN KEY (`schoolId`) REFERENCES `School`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
